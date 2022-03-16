8fa3140b77e9c19a982d31be037957f4d9516aa0
Prateek Tandon <prateek1.tandon@gmail.com>
2022-03-15 21:26:12 -0700

    ocr test

--- card_recognizer/ocr/test/test_ocr_dep.py
+++ card_recognizer/ocr/test/test_ocr_dep.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import easyocr
@@ -6,6 +7,21 @@ import pytesseract
 
 class TestOCRDep(unittest.TestCase):
 
-    def test_easy_ocr(self):
+    def setUp(self) -> None:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.test_image = os.path.join(dir_path, 'data', 'joy_of_data.png')
+
+    def test_easy_ocr(self) -> None:
+        """
+        Test EasyOCr on sample image.
+        """
         easy_ocr_reader = easyocr.Reader(["en"])
-        easy_ocr_reader.readtext(png.name, detail=0)
+        output = easy_ocr_reader.readtext(self.test_image, detail=0)
+        self.assertEqual(output, ['joy', 'of', 'data'])
+
+    def test_pytesseract(self) -> None:
+        """
+        Test PyTesseract on sample image.
+        """
+        output = pytesseract.image_to_string(self.test_image)
+        self.assertEqual(output.strip(), 'joy of data')
