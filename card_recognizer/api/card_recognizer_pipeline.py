a215b3d7dda302950574935c106006a5b6270317
Prateek Tandon <prateek1.tandon@gmail.com>
2022-04-02 13:06:58 -0700

    port to example runner

--- card_recognizer/api/card_recognizer_pipeline.py
+++ card_recognizer/api/card_recognizer_pipeline.py
@@ -4,8 +4,6 @@ from typing import Optional
 
 from algo_ops.ops.text import TextOp
 from algo_ops.pipeline.pipeline import Pipeline
-from natsort import natsorted
-from ocr_ops.dependency import sys_util
 from ocr_ops.framework.op.ffmpeg_op import FFMPEGOp
 from ocr_ops.framework.op.ocr_op import OCRMethod
 from ocr_ops.instances import ocr
@@ -108,40 +106,3 @@ class CardRecognizerPipeline(Pipeline):
                 op.out_path = os.path.join(output_path, 'uncompressed_video_frames')
             if isinstance(op, PullsEstimator):
                 op.output_fig_path = output_path
-
-
-if __name__ == "__main__":
-    pipeline = CardRecognizerPipeline(
-        set_name="Brilliant Stars", mode=Mode.BOOSTER_PULLS_VIDEO
-    )
-    in_dir = os.sep + os.path.join(
-        "media",
-        "borg1",
-        "Borg12TB",
-        "card_recognizer_test_sets",
-        "brilliant_stars_booster_box_3_2022",
-        "new",
-    )
-    """
-    in_file = os.sep + os.path.join("home", "borg1", "Desktop",
-     "Y2Mate.is - Brilliant Stars Booster Box Opening PART 1-t8NtWA2_26M-1080p-1647284353120.mp4")
-    r = pipeline.exec(inp=in_file)
-    with open('tt_pkl.pkl', 'wb') as fout:
-        pickle.dump(r, fout)
-    """
-    videos = natsorted(
-        [
-            os.path.join(in_dir, video)
-            for video in os.listdir(in_dir)
-            if sys_util.is_video_file(video)
-        ]
-    )
-    for video in videos:
-        print(video)
-        results_path = os.path.basename(video)
-        pipeline_pkl_path = os.path.join(results_path, os.path.basename(video) + ".pkl")
-        pipeline.set_output_path(output_path=results_path)
-        result = pipeline.exec(inp=video)
-        pipeline.vis()
-        pipeline.to_pickle(out_pkl_path=pipeline_pkl_path)
-        print(result)
---
+++ example_runner.py
@@ -0,0 +1,42 @@
+import os
+
+from natsort import natsorted
+from ocr_ops.dependency import sys_util
+
+from card_recognizer.api.card_recognizer_pipeline import CardRecognizerPipeline, Mode
+
+if __name__ == "__main__":
+    pipeline = CardRecognizerPipeline(
+        set_name="Brilliant Stars", mode=Mode.BOOSTER_PULLS_VIDEO
+    )
+    in_dir = os.sep + os.path.join(
+        "media",
+        "borg1",
+        "Borg12TB",
+        "card_recognizer_test_sets",
+        "brilliant_stars_booster_box_3_2022",
+        "new",
+    )
+    """
+    in_file = os.sep + os.path.join("home", "borg1", "Desktop",
+     "Y2Mate.is - Brilliant Stars Booster Box Opening PART 1-t8NtWA2_26M-1080p-1647284353120.mp4")
+    r = pipeline.exec(inp=in_file)
+    with open('tt_pkl.pkl', 'wb') as fout:
+        pickle.dump(r, fout)
+    """
+    videos = natsorted(
+        [
+            os.path.join(in_dir, video)
+            for video in os.listdir(in_dir)
+            if sys_util.is_video_file(video)
+        ]
+    )
+    for video in videos:
+        print(video)
+        results_path = os.path.basename(video)
+        pipeline_pkl_path = os.path.join(results_path, os.path.basename(video) + ".pkl")
+        pipeline.set_output_path(output_path=results_path)
+        result = pipeline.exec(inp=video)
+        pipeline.vis()
+        pipeline.to_pickle(out_pkl_path=pipeline_pkl_path)
+        print(result)
