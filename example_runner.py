a885d8a0babebe9cb4850ed5ad1d0194193b3f5b
Prateek <prateek1.tandon@gmail.com>
2023-07-01 09:26:24 -0700

    paldea evolved

--- example_runner.py
+++ example_runner.py
@@ -16,14 +16,14 @@ if __name__ == "__main__":
 
     # init pipeline and paths
     pipeline = CardRecognizer(
-        set_name="Crown Zenith", mode=OperatingMode.BOOSTER_PULLS_VIDEO
+        set_name="Paldea Evolved", mode=OperatingMode.BOOSTER_PULLS_VIDEO
     )
     in_dir = os.sep + os.path.join(
         "media",
         "borg1",
         "Borg12TB",
         "card_recognizer_test_sets",
-        "crown_zenith_regieleki_box_gamestop_1-21-2023",
+        "paldea_evolved_etb_gamenerdz_6-18-2023",
     )
     out_dir = os.sep + os.path.join("media", "borg1", "Borg12TB", "card_rec_results")
     pipeline.set_summary_file(
